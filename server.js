16b117ee9ba4c6390cb46a84d782c4e69acffb3a
Jung No <jungno@gmail.com>
2015-09-11 18:00:52 -0700

    mvc3 model

--- server.js
+++ server.js
@@ -7,14 +7,19 @@ var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require('mongoose');
 
-//start model 
-mongoose.connect('mongodb://localhost/quotes');
-var QuotesSchema = new mongoose.Schema({
-	name: String,
-	quote: String
-})
-var Quote = mongoose.model('Quote', QuotesSchema);
-//ends model
+//MVC3d moved to models
+// //start model 
+// mongoose.connect('mongodb://localhost/quotes');
+// var QuotesSchema = new mongoose.Schema({
+// 	name: String,
+// 	quote: String
+// })
+// var Quote = mongoose.model('Quote', QuotesSchema);
+// //ends model
+
+//MVC3e require
+require('./server/config/mongoose.js');
+
 
 app.use(bodyParser.urlencoded());
 //static content
---
+++ server/models/quote.js
@@ -0,0 +1,12 @@
+//MVCb require
+var mongoose = require('mongoose');
+
+//MVC3c moved from server.js
+//start model 
+// mongoose.connect('mongodb://localhost/quotes');
+var QuotesSchema = new mongoose.Schema({
+	name: String,
+	quote: String
+})
+var Quote = mongoose.model('Quote', QuotesSchema);
+//ends model
